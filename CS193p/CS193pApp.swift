7e94aa47c354b2b5f21791f7ea5d883feb744dd1
keplib <keplib@gmail.com>
2022-11-14 23:55:05 +0100

    Initial Commit

---
+++ CS193p/CS193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS193pApp.swift
+//  CS193p
+//
+//  Created by Balázs Képli on 14/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CS193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
