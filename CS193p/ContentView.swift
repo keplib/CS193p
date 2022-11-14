a61259eb733cb361a8486842935f4a95569dadfa
keplib <keplib@gmail.com>
2022-11-15 00:05:24 +0100

    Test commit

--- CS193p/ContentView.swift
+++ CS193p/ContentView.swift
@@ -9,8 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            
+            Text("Hello, world!")
+                .padding()
+            
+            Text("This is a test!")
+            
+           
+        }
+        
     }
 }
 
